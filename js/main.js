2b94102be71265bd80f58420efa51a64075e1e00
Aleksandra Chernova <shuraorgm@gmail.com>
2022-02-13 14:48:38 +0300

    teacher. step 2

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ WidthWin = $(window).width();
 
 $(document).ready(function () {
     // слайдер спектр возможностей
-    if($("#widespektr").length)
+    if($("#widespektr").length) {
         $("#widespektr").slick({
             dots: false,
             arrows: true,
@@ -20,6 +20,41 @@ $(document).ready(function () {
                 }
             ]
         });
+    }
+    if($("#slider-teachers").length) {
+        $("#slider-teachers").slick({
+            dots: false,
+            arrows: true,
+            infinite: true,
+            speed: 500,
+            autoplay: false,
+            slidesToShow: 5,
+            slidesToScroll: 5,
+            responsive: [
+                {
+                  breakpoint: 1330,
+                  settings: {
+                      slidesToShow: 4,
+                      slidesToScroll: 4
+                  }
+                },
+                {
+                  breakpoint: 1000,
+                  settings: {
+                      slidesToShow: 3,
+                      slidesToScroll: 3
+                  }
+                },
+                {
+                  breakpoint: 480,
+                  settings: {
+                      slidesToShow: 1,
+                      slidesToScroll: 1
+                  }
+                }
+            ]
+        });
+    }
 // END слайдер спектр возможностей
 
 });
