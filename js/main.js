14f1353348a192ac8ae3741e7ea43be9e9ec8e16
Aleksandra Chernova <shuraorgm@gmail.com>
2022-02-13 14:13:45 +0300

    complet spectr. start teacher

--- js/main.js
+++ js/main.js
@@ -9,8 +9,16 @@ $(document).ready(function () {
             arrows: true,
             infinite: true,
             speed: 500,
-            autoplay: false,
-            autoplaySpeed: 500,
+            autoplay: true,
+            autoplaySpeed: 700,
+            responsive: [
+                {
+                    breakpoint: 1024,
+                    setting: {
+                        autoplay: false
+                    }
+                }
+            ]
         });
 // END слайдер спектр возможностей
 
