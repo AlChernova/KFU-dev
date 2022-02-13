7334e708b7ca50574c357d2dc8c5106803b44ce2
Aleksandra Chernova <shuraorgm@gmail.com>
2022-02-13 13:27:40 +0300

    start slider

--- js/main.js
+++ js/main.js
@@ -1,1 +1,20 @@
-"use strict";
+var WidthWin;
+WidthWin = $(window).width();
+
+$(document).ready(function () {
+    // слайдер спектр возможностей
+    if($("#widespektr").length)
+        $("#widespektr").slick({
+            dots: false,
+            arrows: true,
+            infinite: true,
+            speed: 500,
+            autoplay: false,
+            autoplaySpeed: 500,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            responsive: [{breakpoint: 768, settings: {pauseOnHover: !1}}]
+        });
+// END слайдер спектр возможностей
+
+});
